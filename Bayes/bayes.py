b6f495707ce7810436db9728f0e7be79c587439f
孙鹏 <sunpeng1996@aliyun.com>
2016-09-15 12:33:12 +0800

    朴素贝叶斯分类器

---
+++ Bayes/bayes.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+from numpy import *
+
+
+def loadDataSet():
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                   ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0, 1, 0, 1, 0, 1]  # 1 is abusive, 0 not
+    return postingList, classVec
+
+
+def createVocabList(dataSet):
+    '''创建一个包含在所有文档中出现的不重复的词的列表'''
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+
+def setOfWords2Vec(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print "the word: %s is not in my Vocabulary！" % word
+    return returnVec
+
+
+def bagOfWords2Vec(vocabList, inputSet):
+    '''构建词袋模型'''
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+        else:
+            print "the word: %s is not in my Vocabulary！" % word
+    return returnVec
+
+
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    p0Num = ones(numWords)
+    p1Num = ones(numWords)  # change to ones()
+    p0Denom = 2.0
+    p1Denom = 2.0  # change to 2.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    # 取自然对数，可以避免下溢出或者浮点数舍入导致的错误
+    p1Vect = log(p1Num / p1Denom)  # change to log()
+    p0Vect = log(p0Num / p0Denom)  # change to log()
+    return p0Vect, p1Vect, pAbusive
+
+
+def classifyNB(vec2Classify, p0V, p1V, pAb):
+    p1 = sum(vec2Classify * p1V) + log(pAb)
+    p2 = sum(vec2Classify * p0V) + log(pAb)
+    if p1 > p2:
+        return 1
+    else:
+        return 0
+
+
+def testingNB():
+    listOPosts, listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    p0V, p1V, pAb = trainNB0(trainMat, listClasses)
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+    testEntry = ['stupid', 'garbage']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+
+
+def textParse(bigString):  # input is big string, #output is word list
+    import re
+    listOfTokens = re.split(r'\W*', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+
+def spamTest():
+    docList = []
+    classList = []
+    fullText = []
+    for i in range(1, 26):
+        wordList = textParse(open('email/spam/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)  # create vocabulary
+    trainingSet = range(50);
+    testSet = []  # create test set
+    for i in range(10):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del (trainingSet[randIndex])
+    trainMat = [];
+    trainClasses = []
+    for docIndex in trainingSet:  # train the classifier (get probs) trainNB0
+        trainMat.append(bagOfWords2Vec(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:  # classify the remaining items
+        wordVector = bagOfWords2Vec(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+            print "classification error", docList[docIndex]
+    print 'the error rate is: ', float(errorCount) / len(testSet)
+    # return vocabList,fullText
+
+
+if __name__ == '__main__':
+    spamTest()
+    # listOPosts, listClasses = loadDataSet()
+    # myVocabList = createVocabList(listOPosts)
+    # # setOfWords2Vec(myVocabList, listOPosts[0])
+    # trainMat = []
+    # for postinDoc in listOPosts:
+    #     trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    # print trainMat  # List包含List的 0 1 特征向量
+    # p0V, p1V, pAb = trainNB0(trainMat, listClasses)
+    # print "p0V :%s ,p1V:%s ,pAb:%s" % (p0V, p1V, pAb)
